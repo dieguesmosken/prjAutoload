c6203f8ccb16fab1e8310232a65bec2a3d9bc5f0
Matheus Diegues <dmosken2015@gmail.com>
2021-05-13 21:45:30 -0300

    first commit

---
+++ classes/Cliente.php
@@ -0,0 +1,14 @@
+<?php
+
+class Cliente {
+    //atributos
+    public $id;
+    public $nome;
+
+    //metodos
+    public function mostrarDados(){
+        return ($this->id ." ". $this->nome);
+    }
+}
+
+?>
